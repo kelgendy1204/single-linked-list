b414eee1b1292ffe58c5385338cdeaa4ee68baa8
Khaled Saad <elgendy1204@gmail.com>
2020-12-03 13:20:21 +0200

    chore: add travis and code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     clearMocks: true,
+    collectCoverage: true,
     coverageDirectory: 'coverage',
+    collectCoverageFrom: ['src/**/*.ts'],
     preset: 'ts-jest',
     testEnvironment: 'node'
 };
