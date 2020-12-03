20cd832496e69eb13b0a825e727c972b4820fdf5
Khaled Saad <elgendy1204@gmail.com>
2020-12-03 14:26:07 +0200

    chore: make some fixed in package.json

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,13 @@ module.exports = {
     coverageDirectory: 'coverage',
     collectCoverageFrom: ['src/**/*.ts'],
     preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+        }
+    }
 };
